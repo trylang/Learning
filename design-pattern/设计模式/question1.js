2f79ca2c4a2b531046bde4a4af4af5eecf5c512f
langyanping <langyanping@rtmap.com>
2018-09-20 21:16:21 +0800

    学习停车面试问题

--- design-pattern/设计模式/question1.js
+++ design-pattern/设计模式/question1.js
@@ -24,16 +24,47 @@ class Screen {
   }
 }
 
+// 车辆
+class Car {
+  constructor(num) {
+    this.num = num;
+  }
+}
+
 // 停车场
 class Park {
   constructor(floors) {
     this.floors = floors || [];
+    this.camera = new Camera();
+    this.screen = new Screen();
+    this.carList = {}; // 存储摄像头拍摄返回的车辆信 息  
   }
   in(car) {
+    // 通过摄像头获取信息
+    const info = this.camera.shot(car);
+    // 停到第一层的某个停车位
+    const i = parseInt(Math.random() * 100 % 100);
+    const place = this.floors[0].places[i];
+
+    // TODO:
+    place.in();
+    info.place = place;
+
+    // 记录信息
+    this.carList[car.num] = info;
 
   }
   out(car) {
-
+    // 获取信息
+    const info = this.carList[car.num];
+    // 将停车位清空
+    const place = info.place;
+    console.log(place);
+    place.out();
+    // 显示时间
+    this.screen.show(car, info.inTime)
+    // 清空记录
+    delete this.carList;
   }
   emptyNum() { //所有停车场剩余车位
     return this.floors.map(floor => {
@@ -71,5 +102,35 @@ class Place {
   }
 }
 
-// 
+// 测试 ---------------
+// 初始化停车场
+const floors = [];
+for (let i = 0; i < 3; i++) {
+  const places = [];
+  for (let j = 0; j < 100; j++) {
+    places[j] = new Place();
+  }
+  floors[i] = new Floor(i + 1, places);
+}
 
+const park = new Park(floors);
+
+// 初始化车辆
+const car1 = new Car(100);
+const car2 = new Car(202);
+const car3 = new Car(230);
+
+// 执行 ---------------------
+console.log(park.emptyNum());
+
+console.log('第一辆车进入');
+park.in(car1);
+console.log(park.emptyNum());
+
+console.log('第二辆车进入');
+park.in(car2);
+console.log(park.emptyNum());
+
+console.log('第一辆车出');
+park.out(car1);
+console.log(park.emptyNum());
